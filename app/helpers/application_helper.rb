4e85aa0ff0456684e2ef98b07269f07affb0fdff
succi0303 <succi0303@gmail.com>
2014-05-01 20:56:04 +0900

    static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = '体重管理'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end

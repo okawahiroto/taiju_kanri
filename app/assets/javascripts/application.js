5e6753dc3ea3d8269be2d5a265eb7d01ded1bca6
succi0303 <succi0303@gmail.com>
2014-05-07 15:12:51 +0900

    add datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.ui.core
+//= require jquery.ui.datepicker
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap

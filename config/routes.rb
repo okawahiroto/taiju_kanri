cf8dfeaa2f3c8f691e633d3086c7ed15b400a591
succi0303 <succi0303@gmail.com>
2014-06-04 21:29:14 +0900

    error handling2

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/contact' => 'static_pages#contact'
 
   root 'static_pages#index'
+  get '*anything' => 'errors#routing_error'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
